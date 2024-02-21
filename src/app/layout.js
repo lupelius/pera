db509d61014b38de2624abbdaf6ce596d54f5f0f
lupelius <easyhostingltd@gmail.com>
2024-02-21 21:39:28 +0000

    all the details added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Geologica:wght@100..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Geologica:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  /*
-    <Image
-              className="ml-8"
-              src="/pera.png"
-              alt="Pera Lisbon Logo"
-              width={120}
-              height={120}
-              priority
-            />
-  */
+
   const img = 
     <Image
       className="ml-3"
@@ -20,55 +11,66 @@ export default function Home() {
       height={160}
       priority
     />
-  const pe = (url, title, paragraph) => <a
-    href={url}
+  const pe = (array) => <div
     className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-    target="_blank"
-    rel="noopener noreferrer"
   >
     <div className="flex flex-row">
-      <div className="group rounded-lg border border-transparent">
+      <a 
+        className="group rounded-lg border border-transparent"
+        href={array[0].url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <div class="flip-card">
           <div class="flip-card-inner">
             <div class="flip-card-front">
                {img}
             </div>
             <div class="flip-card-back">
-                <p class="title">{title}</p>
-                <p>{paragraph}</p>
+                <p class="title">{array[0].title}</p>
+                <p>{array[0].paragraph}</p>
             </div>
           </div>
         </div>
-      </div>
-      <div className="group rounded-lg border border-transparent ml-8">
+      </a>
+      <a className="group rounded-lg border border-transparent ml-8"
+        href={array[1].url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <div class="flip-card">
           <div class="flip-card-inner">
               <div class="flip-card-front">
                 {img}
-
               </div>
               <div class="flip-card-back">
-                  <p class="title">{title}</p>
-                  <p>{paragraph}</p>
+                  <p class="title">{array[1].title}</p>
+                  <p>{array[1].paragraph}</p>
               </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
     
-    <div class="flip-card mt-8">
+    <a 
+      href={array[2].url}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div class="flip-card mt-8">
       <div class="flip-card-inner">
           <div class="flip-card-front">
             {img}
           </div>
           <div class="flip-card-back">
-              <p class="title">{title}</p>
-              <p>{paragraph}</p>
+              <p class="title">{array[2].title}</p>
+              <p>{array[2].paragraph}</p>
           </div>
       </div>
-    </div>
-  </a>
-  const item = (url, title, paragraph) => <a
+      </div>
+    </a>
+  </div>
+  const item = (url, title, paragraph, bg) => <a
     href={url}
     className={`first-left-margin space-top group rounded-lg border border-transparent px-5 py-4 transition-colors`}
     target="_blank"
@@ -79,7 +81,7 @@ export default function Home() {
           <div class="flip-card-front">
             {img}
           </div>
-          <div class="flip-card-back">
+          <div class="flip-card-back" style={{background: bg}}>
               <p class="title">{title}</p>
               <p>{paragraph}</p>
           </div>
@@ -89,10 +91,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between open-sans-normal text-sm lg:flex">
         <p className="ml-4 fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Life is ours, we live it&nbsp;
-          <code className="font-mono font-bold">our way</code>
+          <code className="font-mono open-sans-bold ">our way</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -106,23 +108,17 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         
-          {pe("https://peralisboa.com", "Take me to church", "Or shopping")}
-          {item("https://peralisboa.com", "Take me to church", "Or somewhere else", true)}
-          {item("https://peralisboa.com", "Take me to church", "Or not")}
-          {item("https://peralisboa.com", "Take me to church", "Or maybe")}
+          {pe([
+            {url: "https://peralisboa.com/pages/about", title: "About us", paragraph: "about you"},
+            {url: "https://peralisboa.com", title: "Take me to church", paragraph: "Or shopping"},
+            {url: "https://peralisboa.com/pages/events", title: "Events are fun", paragraph: "right?"},
+          ])}
+          {item("https://peralisboa.com/pages/workshops", "Workshops", "Or not to workshops", "#960018")}
+          {item("https://peralisboa.com/pages/art", "Art is for art", "Or for humans", "#120A8F")}
+          {item("https://www.google.com/maps/place/Pera+Lisboa/@38.7093854,-9.1507565,17z/data=!3m1!4b1!4m6!3m5!1s0xd193592b3b63555:0x47a08d5d9b063ba1!8m2!3d38.7093854!4d-9.1507565!16s%2Fg%2F11v64zz_84?entry=ttu", 
+            "Come to us,", "perhaps", "#FFD700")}
 
       </div>
     </main>
